39e2a2a02b7a2e0380fb79d10884395032c99b83
gusarovden <gusarovden1@gmail.com>
2026-03-22 22:58:43 +0300

    config

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ MY_COOKIES = {
 API_URL = 'https://web-agr.chitai-gorod.ru/web/api/v2/'
 MY_HEADERS = {
     "Accept": "application/json",
-    "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJodHRwczovL3VzZXItcmlnaHQiLCJzdWIiOjIzMjQ4OTM0LCJpYXQiOjE3NzQxOTU2ODksImV4cCI6MTc3NDE5OTI4OSwidHlwZSI6MjAsImp0aSI6IjAxOWQxNjRkLWNhMDQtNzA4NS1iZmM5LWZkODhmOTAzYWVkZiIsInJvbGVzIjoxMH0.ooM1Pyp_9yHlCObOZJqPx5x3iVd1vqH-78WvPROPKXFFEaWwhUE4A851ThNbz1sZMeKvQuOTWh8iV-JuqUTXAAJBmntJebnTZDiqWWufDgm8xy9LrmmVGZTLWk80JIdoRy72EideNxOhojORBeisGEdtUulGdOFLbhSBkrg1gtC3ZcMGLLXg-hCs8ctmzaNiSfVPB4dzxycqVA31-SY3BWuSRr6X3uM3OIzjM2v8Vh1fKjwO9MP1xMzZY1t6TR45MtoQunpVduYZnFPZKawLGIby0mjgpxmKgFZ6ldmKrrNN-b8OkZFHf-00rQJi_xGohLVjAEN82qoDgwR4VHPJOg",
+    "Authorization": "Bearer ваш token",
     'Content-Type': 'application/json',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 YaBrowser/26.3.0.0 Safari/537.36'
 }
